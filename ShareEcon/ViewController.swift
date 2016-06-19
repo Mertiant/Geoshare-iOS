df0240aba772fd5b2d391df3d4fda26a8a926b75
Numaer Zaker <znzaker@gmail.com>
2016-06-18 20:15:02 -0400

    push changes

--- ShareEcon/ViewController.swift
+++ ShareEcon/ViewController.swift
@@ -15,11 +15,9 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var textLabel: UILabel!
     
     @IBOutlet weak var tableView: UITableView!
-    let data = ["Shovel", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
-        "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
-        "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
-        "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
-        "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    var data = ["Shovel"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
@@ -39,16 +37,14 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
     @IBAction func changedTab(sender: UISegmentedControl) {
 
         if (tabControll.selectedSegmentIndex == 0){
         textLabel.text = "Borrow"
             tableView.hidden = true
-            
-            
-            
+            data.append("Dicks")
+            tableView.reloadData()
         }
         else if(tabControll.selectedSegmentIndex == 1){
         
